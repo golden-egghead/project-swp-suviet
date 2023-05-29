0fc4e3c1411fa759772e526fd6284a0866ffb066
NhanNguyen8080 <boiboii162122@gmail.com>
2023-05-29 13:58:07 +0700

    update model pair of quizz

--- SuViet/src/main/java/com/example/SuViet/model/PairOfQuizz.java
+++ SuViet/src/main/java/com/example/SuViet/model/PairOfQuizz.java
@@ -16,10 +16,18 @@ public class PairOfQuizz {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int pairOfQuizzID;
-    private String quesion;
+    private String question;
     private String answer;
     private int gameSlicePoint;
     private boolean enabled;
+
+    public PairOfQuizz(int pairOfQuizzID, String question, String answer, int gameSlicePoint, boolean enabled) {
+        this.pairOfQuizzID = pairOfQuizzID;
+        this.question = question;
+        this.answer = answer;
+        this.gameSlicePoint = gameSlicePoint;
+        this.enabled = enabled;
+    }
 
     @JsonIgnore
     @ManyToOne
