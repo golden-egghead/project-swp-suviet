f8e08e432a8ccc123cc89bf99e2ae1b8191c3a05
NhanNguyen8080 <boiboii162122@gmail.com>
2023-05-30 09:12:17 +0700

    update code

--- SuViet/src/main/java/com/example/SuViet/controller/VideoController.java
+++ SuViet/src/main/java/com/example/SuViet/controller/VideoController.java
@@ -5,9 +5,10 @@ import com.example.SuViet.model.Video;
 import com.example.SuViet.service.VideoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,8 +29,4 @@ public class VideoController {
     }
 
 
-    @PostMapping("/insert")
-    public ResponseEntity<ResponseObject> insertAVideo(@RequestBody Video video) {
-
-    }
 }
