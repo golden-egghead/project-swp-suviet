5bc5939e2fa6666f6e2779edd4940b39ac7b9774
NhanNguyen8080 <boiboii162122@gmail.com>
2023-05-30 16:56:28 +0700

    update PeriodRepository and PeriodController

--- SuViet/src/main/java/com/example/SuViet/repository/PeriodRepository.java
+++ SuViet/src/main/java/com/example/SuViet/repository/PeriodRepository.java
@@ -1,2 +1,9 @@
-package com.example.SuViet.repository;public interface PeriodRepository {
+package com.example.SuViet.repository;
+
+import com.example.SuViet.model.Period;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PeriodRepository extends JpaRepository<Period, Integer> {
 }
