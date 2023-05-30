2b85af8cf246ccd29763131f198b9f6d4af6c61b
NhanNguyen8080 <boiboii162122@gmail.com>
2023-05-30 09:08:14 +0700

    Update code

--- SuViet/src/main/java/com/example/SuViet/repository/VideoRepository.java
+++ SuViet/src/main/java/com/example/SuViet/repository/VideoRepository.java
@@ -3,5 +3,8 @@ package com.example.SuViet.repository;
 import com.example.SuViet.model.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VideoRepository extends JpaRepository<Video, Integer> {
+    public List<Video> findAllByEnabled(boolean enabled);
 }
