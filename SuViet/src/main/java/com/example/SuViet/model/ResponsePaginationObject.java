e6b8c9fefc97aa0b1249bb1849c5b3b4a5a877c1
NhanNguyen8080 <boiboii162122@gmail.com>
2023-05-31 16:42:33 +0700

    add ResponsePaginationObject.java

--- SuViet/src/main/java/com/example/SuViet/model/ResponsePaginationObject.java
+++ SuViet/src/main/java/com/example/SuViet/model/ResponsePaginationObject.java
@@ -1,18 +1,24 @@
 package com.example.SuViet.model;
 
-public class ResponseVieoObject {
+public class ResponsePaginationObject {
     private String status;
     private String message;
-    private String totalNumber;
+    private int page;
+    private int per_page;
+    private int total;
+    private double total_pages;
     private Object data;
 
-    public ResponseVieoObject() {
+    public ResponsePaginationObject() {
     }
 
-    public ResponseVieoObject(String status, String message, String totalNumber, Object data) {
+    public ResponsePaginationObject(String status, String message, int page, int per_page, int total, double total_pages, Object data) {
         this.status = status;
         this.message = message;
-        this.totalNumber = totalNumber;
+        this.page = page;
+        this.per_page = per_page;
+        this.total = total;
+        this.total_pages = total_pages;
         this.data = data;
     }
 
@@ -32,12 +38,36 @@ public class ResponseVieoObject {
         this.message = message;
     }
 
-    public String getTotalNumber() {
-        return totalNumber;
+    public int getPage() {
+        return page;
     }
 
-    public void setTotalNumber(String totalNumber) {
-        this.totalNumber = totalNumber;
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPer_page() {
+        return per_page;
+    }
+
+    public void setPer_page(int per_page) {
+        this.per_page = per_page;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public double getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(double total_pages) {
+        this.total_pages = total_pages;
     }
 
     public Object getData() {
@@ -48,13 +78,5 @@ public class ResponseVieoObject {
         this.data = data;
     }
 
-    @Override
-    public String toString() {
-        return "ResponseVieoObject{" +
-                "status='" + status + '\'' +
-                ", message='" + message + '\'' +
-                ", totalNumber='" + totalNumber + '\'' +
-                ", data=" + data +
-                '}';
-    }
+
 }
