4e7419f89c42fba90ed3aa60ecaeceebd8d436cc
NhanNguyen8080 <boiboii162122@gmail.com>
2023-06-20 22:43:50 +0700

    Update model.period and add 2 insert data in period files

--- SuViet/src/main/java/com/example/SuViet/model/Period.java
+++ SuViet/src/main/java/com/example/SuViet/model/Period.java
@@ -1,5 +1,6 @@
 package com.example.SuViet.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import jakarta.persistence.*;
@@ -22,12 +23,25 @@ public class Period {
     @Column(nullable = false)
     private boolean enabled;
 
-    public Period(int periodID, String periodName, boolean enabled) {
+    @Column(name = "Image", columnDefinition = "ntext", nullable = true)
+    private String image;
+
+    @Column(name = "Description", columnDefinition = "nvarchar", length = Integer.MAX_VALUE, nullable = true)
+    private String description;
+
+    @Column(name = "year", columnDefinition = "nvarchar", length = 50, nullable = true)
+    private String year;
+
+    public Period(int periodID, String periodName, boolean enabled, String image, String description, String year) {
         this.periodID = periodID;
         this.periodName = periodName;
         this.enabled = enabled;
+        this.image = image;
+        this.description = description;
+        this.year = year;
     }
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "periods")
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
@@ -48,11 +62,13 @@ public class Period {
     @EqualsAndHashCode.Exclude
     private Collection<HistoricalItem> historicalItems;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "periods")
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     private Collection<Video> videos;
 
+    @JsonIgnore
     @ManyToMany(mappedBy = "periods")
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
