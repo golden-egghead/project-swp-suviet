2e36f969c22910eab84224ddb8ecb665fe92492d
sonlc02 <congson2002@gmail.com>
2023-06-22 12:23:42 +0700

    update period

--- SuViet/src/main/java/com/example/SuViet/controller/PeriodController.java
+++ SuViet/src/main/java/com/example/SuViet/controller/PeriodController.java
@@ -7,15 +7,13 @@ import com.example.SuViet.service.PeriodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/periods")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PeriodController {
     private final PeriodService periodService;
 
