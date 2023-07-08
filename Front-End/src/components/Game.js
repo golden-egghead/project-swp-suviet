d6c4e21dca37ff707a352c7999636aece76dc39d
Dương Thị Trúc Linh <duongthitruclinh257@gmail.com>
2023-07-08 12:04:55 +0700

    delete FE folder

--- Front-End/src/components/Game.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Game() {
-  return (
-    <div>Game</div>
-  )
-}
--- Front-End/src/components/Map/MapComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const MapComponent = () => {
-  return (
-  );
-};
-
-export default MapComponent;
